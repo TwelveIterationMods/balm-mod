06084eaf814d5ebb337a73944e0788cab937a071
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-28 17:49:16 +0100

    fix: Fix last commit

--- common/src/main/java/yourname/mods/yourmod/network/ModNetworking.java
+++ common/src/main/java/yourname/mods/yourmod/network/ModNetworking.java
@@ -5,10 +5,12 @@ import net.blay09.mods.balm.api.network.SyncConfigMessage;
 import yourname.mods.yourmod.YourModConfig;
 import yourname.mods.yourmod.network.protocol.ClientboundConfigMessage;
 
+import static yourname.mods.yourmod.YourMod.id;
+
 public class ModNetworking {
 
     public static void initialize(BalmNetworking networking) {
-        SyncConfigMessage.register(ClientboundConfigMessage.TYPE,
+        SyncConfigMessage.register(id("config"),
                 ClientboundConfigMessage.class,
                 ClientboundConfigMessage::new,
                 YourModConfig.class,
