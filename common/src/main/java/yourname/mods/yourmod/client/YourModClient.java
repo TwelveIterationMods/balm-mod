a2ad695c7c8d46af0b33c404617ce72554938f73
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-28 16:27:15 +0100

    refactor!: Simplify template mod

--- common/src/main/java/yourname/mods/yourmod/client/YourModClient.java
+++ common/src/main/java/yourname/mods/yourmod/client/YourModClient.java
@@ -4,9 +4,6 @@ import net.blay09.mods.balm.api.client.BalmClient;
 
 public class YourModClient {
     public static void initialize() {
-        ModScreens.initialize(BalmClient.getScreens());
-        ModRenderers.initialize(BalmClient.getRenderers());
-        ModModels.initialize(BalmClient.getModels());
         ModKeyMappings.initialize();
     }
 }
