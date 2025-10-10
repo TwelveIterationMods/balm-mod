4368f9985e0ea2ff95ab82affccb2747bd20ce7e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-10-10 08:49:28 +0200

    fix: Fix Fabric datagen

--- fabric/src/main/java/yourname/mods/yourmod/fabric/datagen/ModBlockTagProvider.java
+++ fabric/src/main/java/yourname/mods/yourmod/fabric/datagen/ModBlockTagProvider.java
@@ -3,20 +3,18 @@ package yourname.mods.yourmod.fabric.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.level.block.Block;
 import yourname.mods.yourmod.block.ModBlocks;
 import yourname.mods.yourmod.tag.ModBlockTags;
 
 import java.util.concurrent.CompletableFuture;
 
-public class ModBlockTagProvider extends FabricTagProvider<Block> {
+public class ModBlockTagProvider extends FabricTagProvider.BlockTagProvider {
     public ModBlockTagProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
-        super(output, Registries.BLOCK, registriesFuture);
+        super(output, registriesFuture);
     }
 
     @Override
     protected void addTags(HolderLookup.Provider arg) {
-        getOrCreateTagBuilder(ModBlockTags.YOUR_TAG).add(ModBlocks.yourBlock);
+        valueLookupBuilder(ModBlockTags.YOUR_TAG).add(ModBlocks.yourBlock);
     }
 }
--- fabric/src/main/java/yourname/mods/yourmod/fabric/datagen/ModItemTagProvider.java
+++ fabric/src/main/java/yourname/mods/yourmod/fabric/datagen/ModItemTagProvider.java
@@ -3,20 +3,18 @@ package yourname.mods.yourmod.fabric.datagen;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.item.Item;
 import yourname.mods.yourmod.item.ModItems;
 import yourname.mods.yourmod.tag.ModItemTags;
 
 import java.util.concurrent.CompletableFuture;
 
-public class ModItemTagProvider extends FabricTagProvider<Item> {
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
     public ModItemTagProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
-        super(output, Registries.ITEM, registriesFuture);
+        super(output, registriesFuture);
     }
 
     @Override
     protected void addTags(HolderLookup.Provider lookup) {
-        getOrCreateTagBuilder(ModItemTags.YOUR_TAG).add(ModItems.yourItem);
+        valueLookupBuilder(ModItemTags.YOUR_TAG).add(ModItems.yourItem);
     }
 }
