9b26e20f0b9bef236d1a3ae3a292b47dadbab48f
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-22 18:57:24 +0100

    feat: Update to Minecraft 26.1

--- neoforge/src/main/java/yourname/mods/yourmod/neoforge/NeoForgeYourMod.java
+++ neoforge/src/main/java/yourname/mods/yourmod/neoforge/NeoForgeYourMod.java
@@ -3,14 +3,15 @@ package yourname.mods.yourmod.neoforge;
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import yourname.mods.yourmod.YourMod;
 
 @Mod(YourMod.MOD_ID)
 public class NeoForgeYourMod {
 
-    public NeoForgeYourMod(IEventBus modEventBus) {
-        final var context = new NeoForgeLoadContext(modEventBus);
+    public NeoForgeYourMod(ModContainer modContainer, IEventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, modEventBus);
         Balm.initializeMod(YourMod.MOD_ID, context, YourMod::initialize);
     }
 }
--- neoforge/src/main/java/yourname/mods/yourmod/neoforge/client/NeoForgeYourModClient.java
+++ neoforge/src/main/java/yourname/mods/yourmod/neoforge/client/NeoForgeYourModClient.java
@@ -4,6 +4,7 @@ import net.blay09.mods.balm.client.BalmClient;
 import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import yourname.mods.yourmod.YourMod;
 import yourname.mods.yourmod.client.YourModClient;
@@ -11,8 +12,8 @@ import yourname.mods.yourmod.client.YourModClient;
 @Mod(value = YourMod.MOD_ID, dist = Dist.CLIENT)
 public class NeoForgeYourModClient {
 
-    public NeoForgeYourModClient(IEventBus modEventBus) {
-        final var context = new NeoForgeLoadContext(modEventBus);
+    public NeoForgeYourModClient(ModContainer modContainer, IEventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, modEventBus);
         BalmClient.initializeMod(YourMod.MOD_ID, context, YourModClient::initialize);
     }
 }
